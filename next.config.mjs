33dd6ba29ee7aebe9cdccfc8c8383ec884aa7bf2
White-D-coder <futuristicthought07@gmail.com>
2026-01-04 15:45:47 +0530

    Update next.config.mjs: Automated sync 2026-01-04T10:15:47.366Z

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const nextConfig = {
 
-    webpack: (config) => {
-        // Fallback for webpack if jsconfig isn't picked up
-        config.resolve.alias['@'] = __dirname;
-        return config;
-    },
+
 };
 
 export default nextConfig;
