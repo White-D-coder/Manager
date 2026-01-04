9c82f42382ca9d7006eb9cd9f571af679a0bef67
White-D-coder <futuristicthought07@gmail.com>
2026-01-04 12:43:23 +0530

    Update app/actions/pexels.ts: Automated sync 2026-01-04T07:13:23.492Z

--- app/actions/pexels.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { PexelsService } from "@/lib/api/pexels";
-
-export async function searchStockVideos(query: string) {
-    return await PexelsService.searchVideos(query);
-}
